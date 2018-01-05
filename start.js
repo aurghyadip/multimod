8645530158d1b0ce402281d90549c6aba1721fd3
Aurghyadip <adkundu@gmail.com>
2018-01-05 10:02:37 +0530

    Steam upload

--- start.js
+++ start.js
@@ -1,64 +1,21 @@
 let _modPath;
 
-//Let's not type GetRootScope everytime we want to access root scope
-rs = GetRootScope();
+exports.initialize = (modPath) =>{                     
+    _modPath = modPath;
+};
 
-// Check if the mod is loaded
-Helpers.ConsoleInfo('[MOD] Project Mod loaded');
-
-exports.initialize = (modPath) => {
-	_modPath = modPath;
-
-
-
-	Modding.setMenuItem({
-		name: 'projectmod',
-		tooltim: 'Project Mod',
-		tooltipPosition: 'top',
-		faIcon: 'fa-bolt',
-		badgeCount: 0
-	});
-
-	exports.views = [{
-		name: 'projectmod',
-		viewPath: _modPath + 'view.html',
-		controller: function ($rootScope) {
-			
-			this.products_list = rs.settings.products;
-			this.progress_list = rs.settings.progress; 
-
-			// Max Features Start
-			this.MaxProjectFeatures = () => {
-				$rootScope.confirm('Are you sure?', 'Are you sure you want to max all the feauters?', () => {
-					for (x in rs.settings.featureInstances) {
-    					rs.settings.featureInstances[x].quality.current = rs.settings.featureInstances[x].quality.max;
-    					rs.settings.featureInstances[x].efficiency.current = rs.settings.featureInstances[x].efficiency.max;
-					}
-				});
-			}
-			// Max Features End
-
-			this.AddThousandUsers = (product_id) => {
-				for (var key in rs.settings.progress.products) {
-					if(key == product_id) {
-						rs.settings.progress.products[key].users.total += 1000;
-					}
-				}
-			}
-			this.AddTenThousandUsers = (product_id) => {
-				for (var key in rs.settings.progress.products) {
-					if(key == product_id) {
-						rs.settings.progress.products[key].users.total += 10000;
-					}
-				}
-			}
-			this.AddHundredThousandUsers = (product_id) => {
-				for (var key in rs.settings.progress.products) {
-					if(key == product_id) {
-						rs.settings.progress.products[key].users.total += 100000;
-					}
-				}
-			}
+exports.onLoadGame  = settings => {
+	settings.office.workstations.forEach(ws => {
+		if (ws.employee != null) {
+			ws.employee.speed = 1000
 		}
-	}]
-}
+	});
+};
+
+exports.onNewDay = settings => {
+	settings.office.workstations.forEach(ws => {
+		if (ws.employee != null) {
+			ws.employee.speed = 1000
+		}
+	});
+};
